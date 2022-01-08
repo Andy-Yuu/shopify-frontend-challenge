2e629ffde6b9d53f0348d67e25cf47a50f88bf9b
Andy Yu <yuandy1031@gmail.com>
2022-01-08 13:28:30 -0500

    chg: update test and readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial page', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Spacestagram/i)).toBeInTheDocument();
+  expect(screen.getByText(/Fetch Random Astronomy Picture of the Day/i)).toBeInTheDocument();
 });
